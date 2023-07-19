57251c77cb1c651183015f4a5211319a989ccb5a
Allison Blumenthal <blumen.allison@gmail.com>
2023-07-18 22:22:44 -0400

    added retrieve and list methods for categories

--- bangazonapi/views/__init__.py
+++ bangazonapi/views/__init__.py
@@ -3,3 +3,4 @@ from .register import check_user, register_user
 from .product import ProductView
 from .order import OrderView
 from .order_product import OrderProductView
+from .category import CategoryView
